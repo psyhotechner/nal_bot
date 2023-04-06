bd11c4bc4bb38f766333db1f5fa456b6d6a500b1
Phil Tsentner <philtsentner@icloud.com>
2023-04-06 23:34:40 +1000

    все четко

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nal_bot
+# create database.py then import sqlite3==2.6.0
+
+# create SQLite connector and others in database.py file
---
+++ database.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+#создаем коннект с уже созданной базой данных
+conn = sqlite3.connect(r'C:\Users\develop\Desktop\SQLite_DB')
